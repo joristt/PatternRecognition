e48edd323e89da3c552063b1b85a0c340e2fc98a
Joris ten Tusscher <joristt@gmail.com>
2019-01-18 15:43:26 +0100

    v2

--- workspace/analyse2.py
+++ workspace/analyse2.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy  as np
 import os
 import matplotlib.pyplot as plt
-from time import time
+from time                  import time
+from sklearn.preprocessing import MinMaxScaler
 
 # In debug mode, only a small portion of the 629.145.481 rows from train.csv
 # will be read.
@@ -20,18 +21,26 @@ s = time()
 print("Loading data into RAM...")
 data = pd.read_csv(
 	train_path,
-	nrows=10e7 if DEBUG else None, # nrows=None causes all lines to be read.
+	nrows=10e6 if DEBUG else None, # nrows=None causes all lines to be read.
 	dtype = {
-		"acoustic_data"   : np.int16,  # The seismic signal
+        # The seismic signal is int16, but we normalise it later on and
+        # therefore we load the acoustic data as floats.
+		"acoustic_data"   : np.float64,
 		"time_to_failure" : np.float64 # The time (s) till next earthquake.
-
 	}
-).values # Convert pd.dataframe to np.ndarray
-
-scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
+)
 
 print("Loaded data in %s seconds." % (time() - s))
+
+s = time()
+print("Normalising data...")
+
+# Convert pd.dataframe to normalised np.ndarray in range [0,1]
+scaler = MinMaxScaler(feature_range=(0, 1))
+data = scaler.fit_transform(data)
+
+print("Normalised data in %s seconds." % (time() - s))
+
 
 # Calculate summary statistics per time step.
 def summary_statistics(z):
@@ -78,17 +87,15 @@ def sample(data, size=100000):
 
 # For a given ending position "u_bound", we split the last 150.000 values of "x" into 150 pieces of length 1000 each.
 # From each piece, 16 features are extracted. This results in a feature matrix of dimension (150 time steps x 16 features). 
-def features(data, u_bound=None, nr_subsets=150, subset_size=1000, mean=None, std=None):
+def features(data, u_bound=None, nr_subsets=150, subset_size=1000):
     nr_values = nr_subsets * subset_size
     u_bound   = u_bound or len(data)
     l_bound   = u_bound - nr_values
-
-    assert l_bound >= 0
-    assert mean != None != std
-
+    if l_bound < 0:
+        l_bound = 0
+        u_bound = 149999
     # Reshaping and standardize approximately
-    l = (data[l_bound:u_bound].reshape(nr_subsets, -1) - mean) / std
-    
+    l = data[l_bound:u_bound].reshape(nr_subsets, -1)
     # Extracts features of sequences of full length 1000, of the last 100 values and finally also 
     # of the last 10 observations. 
     return np.c_[
@@ -99,44 +106,31 @@ def features(data, u_bound=None, nr_subsets=150, subset_size=1000, mean=None, st
     ]
 
 # We call "summary_statistics" three times, so the total number of features is 3 * 5 + 1 (last value) = 16
-nr_features = 16 
-
-# Calculate the mean and standard deviation of a sample of the acoustic values.
-s           = sample(data[:,0])
-sample_mean = s.mean()
-sample_std  = s.std()
-
+nr_features = 16
 
 # The generator randomly selects "batch_size" ending positions of sub-time series. For each ending position,
 # the "time_to_failure" serves as target, while the features are created by the function "features".
 def generator(data):
-
     l_bound     = 0
     u_bound     = len(data) - 1
     batch_size  = 32
     n_steps     = 150
     step_length = 1000
-
     while True:
         # Pick indices of ending positions
         rows = np.random.randint(l_bound + n_steps * step_length, u_bound, size=batch_size)
-
         # Initialize feature matrices and targets
         samples = np.zeros((batch_size, n_steps, nr_features))
         targets = np.zeros(batch_size, )
-        
         for j, row in enumerate(rows):
             samples[j] = features(
                 data[:, 0],
                 u_bound     = row,
                 nr_subsets  = n_steps,
-                subset_size = step_length,
-                mean        = sample_mean,
-                std         = sample_std
+                subset_size = step_length
             )
             targets[j] = data[row, 1]
         yield samples, targets
-        
 
 train_gen = generator(data)
 valid_gen = generator(data)
@@ -166,11 +160,83 @@ model.compile(optimizer=adam(lr=0.0005), loss="mae")
 
 history = model.fit_generator(train_gen,
                               steps_per_epoch=1000,#n_train // batch_size,
-                              epochs=10,
+                              epochs=1,
                               verbose=1,
                               callbacks=cb,
                               validation_data=valid_gen,
                               validation_steps=100)#n_valid // batch_size)
+
+submission = pd.read_csv('data/sample_submission.csv', index_col='seg_id', dtype={"time_to_failure": np.float32})
+
+# Load each test data, create the feature matrix, get numeric prediction
+for i, seg_id in enumerate(submission.index):
+    print(i)
+    seg = pd.read_csv('data/test/' + seg_id + '.csv')
+    x = seg['acoustic_data'].values
+    submission.time_to_failure[i] = model.predict(np.expand_dims(features(x), 0))
+
+
+
+
+
+# Invert scaling of submission.time_to_failure
+ttfs = np.expand_dims(submission.time_to_failure.values, 1)
+ttfs = np.append(ttfs, ttfs, axis=1)
+ttfs = scaler.inverse_transform(ttfs)
+ttfs = np.delete(ttfs, 0, 1)
+ttfs = np.clip(ttfs, 0, a_max=None)
+submission.time_to_failure = ttfs
+
+
+submission.to_csv("data/our_submission.csv")
+
+
+# loss = model.evaluate_generator(valid_gen, steps=100)
+# loss2 = scaler.inverse_transform(np.array([[0,loss]]))[0][1]
+
+
+
+
+
+
+# ss = pd.read_csv(
+#     "data/sample_submission.csv",
+#     dtype = {
+#         # The seismic signal is int16, but we normalise it later on and
+#         # therefore we load the acoustic data as floats.
+#         "acoustic_data"   : np.float64,
+#         "time_to_failure" : np.float64 # The time (s) till next earthquake.
+#     })
+
+# for index, row in ss.iterrows():
+#     seg = row["seg_id"]
+
+#     valData = pd.read_csv(
+#         "data/test/" + seg + ".csv",
+#         dtype = {
+#             # The seismic signal is int16, but we normalise it later on and
+#             # therefore we load the acoustic data as floats.
+#             "acoustic_data"   : np.float64
+#         }
+#     )
+
+#     valData = np.append(valData.values, np.zeros((150000,1)), axis=1)
+#     valData = scaler.transform(valData)
+#     valData = np.delete(valData, 1, 1)
+
+#     valGen = generator(valData)
+
+#     loss = model.predict_generator(valGen, steps=100)
+
+#     break
+
+
+
+
+
+
+
+
 
 # # Visualize accuracies
 # import matplotlib.pyplot as plt
