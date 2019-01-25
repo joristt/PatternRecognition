1b6894f3772bd43de8fca3253b676afacc24873d
Joris ten Tusscher <joristt@gmail.com>
2019-01-26 00:23:09 +0100

    cnns...

--- workspace/analyse2.py
+++ workspace/analyse2.py
@@ -19,14 +19,14 @@ DEBUG = False
 data_dir = "./data/"
 train_path = data_dir + "train.csv"
 
-conn = sqlite3.connect(data_dir + "train.db")
-# conn.text_factory = str
-cur = conn.cursor()
+# conn = sqlite3.connect(data_dir + "train.db")
+# # conn.text_factory = str
+# cur = conn.cursor()
 
-# A REAL in SQLite is an 8byte float, so we don't lose precision.
-def get_rows(l_bound):
-    cur.execute("SELECT * FROM train_table limit %s, %s" % (l_bound, l_bound + num - 1))
-    return cur.fetchall()
+# # A REAL in SQLite is an 8byte float, so we don't lose precision.
+# def get_rows(l_bound):
+#     cur.execute("SELECT * FROM train_table limit %s, %s" % (l_bound, l_bound + num - 1))
+#     return cur.fetchall()
 
 
 
@@ -53,148 +53,291 @@ def get_rows(l_bound):
 
 s = time()
 print("Loading data into RAM...")
-d = pd.read_csv(
-	train_path,
-	nrows=10e5 if DEBUG else None, # nrows=None causes all lines to be read.
-	dtype = {
-        # The seismic signal is int16, but we normalise it later on and
-        # therefore we load the acoustic data as floats.
-		"acoustic_data"   : np.float64,
-		"time_to_failure" : np.float64 # The time (s) till next earthquake.
-	}
-)
+# d = pd.read_csv(
+# 	train_path,
+# 	nrows=10e5 if DEBUG else None, # nrows=None causes all lines to be read.
+# 	dtype = {
+#         # The seismic signal is int16, but we normalise it later on and
+#         # therefore we load the acoustic data as floats.
+# 		"acoustic_data"   : np.float64,
+# 		"time_to_failure" : np.float64 # The time (s) till next earthquake.
+# 	}
+# )
+d = np.load("./data/train.npy")
 print("Loaded data in %s seconds." % (time() - s))
 
-# # s = time()
-# # print("Normalising data...")
-# # Convert pd.dataframe to normalised np.ndarray in range [0,1]
-# # scaler = MinMaxScaler(feature_range=(0, 1))
-# # d = scaler.fit_transform(d)
-# # print("Normalised data in %s seconds." % (time() - s))
+# s = time()
+# print("Normalising data...")
+# Convert pd.dataframe to normalised np.ndarray in range [0,1]
+# scaler = MinMaxScaler(feature_range=(0, 1))
+# d = scaler.fit_transform(d)
+# print("Normalised data in %s seconds." % (time() - s))
 
-# # Split into train and test data.
-# # split_i    = int((d.shape[0]) * 0.8)
-# # train_data = d[:split_i]
-
+# Split into train and test data.
+# split_i    = int((d.shape[0]) * 0.8)
+# train_data = d[:split_i]
 # d = d.values
 
 # scalers = [MinMaxScaler(feature_range=(0, 1)) for i in range(2)]
 # scalers[0].fit_transform(d.values[:,0])
 # scalers[1].fit_transform(d.values[:,1])
 
-# # Calculate summary statistics per time step.
-# def summary_statistics(z):
-# 	return np.c_[z.mean(axis=1), 
-# 	             np.median(np.abs(z), axis=1),
-# 	             z.std(axis=1), 
-# 	             z.max(axis=1),
-# 	             z.min(axis=1)]
+# Calculate summary statistics per time step.
+def summary_statistics(z):
+	return np.c_[z.mean(axis=1), 
+	             np.median(np.abs(z), axis=1),
+	             z.std(axis=1), 
+	             z.max(axis=1),
+	             z.min(axis=1)]
 
-# def stats(z):
-#     return np.c_[
-#         z.mean(axis=1),
-#         z.max(axis=1),
-#         z.min(axis=1),
-#         z.var(axis=1)
-#     ]
+def stats(z):
+    return np.c_[
+        z.mean(axis=1),
+        z.max(axis=1),
+        z.min(axis=1),
+        z.var(axis=1)
+    ]
 
-# # Calculates list of eartquake moments. The list contains tuples. Every tuple
-# # contains two indices. The first index is the last moment before the earth
-# # quake, the second index is always simply first index + 1, and is therefore
-# # the first moment after the earth quake.
-# def quake_moments(data):
-# 	ms = np.where(data[1:,1] > data[:-1,1])[0]
-# 	return zip(ms, ms+1)
+# Calculates list of eartquake moments. The list contains tuples. Every tuple
+# contains two indices. The first index is the last moment before the earth
+# quake, the second index is always simply first index + 1, and is therefore
+# the first moment after the earth quake.
+def quake_moments(data):
+	ms = np.where(data[1:,1] > data[:-1,1])[0]
+	return zip(ms, ms+1)
 
-# # def plott(data, l_bound, u_bound, step_size):
-# # 	smpl       = data[l_bound:u_bound:step_size]
+# def plott(data, l_bound, u_bound, step_size):
+# 	smpl       = data[l_bound:u_bound:step_size]
 
-# # 	# print(np.where(data[,0] > 800))
+# 	# print(np.where(data[,0] > 800))
 
-# # 	quake_moms = quake_moments(data[l_bound:u_bound])
-# # 	# x-axis of plot is time offset, which is the sum of all time_to_failure
-# # 	# values of the first moments after the earth quakes.
-# # 	dur = data[0,1] + sum([data[qm,1] for _,qm in quake_moms])
-# # 	y_vals = np.linspace(0, dur, len(smpl))
-# # 	ts = pd.Series(smpl[l_bound:u_bound,0], index=y_vals)
-# # 	ts.plot()
-# # 	plt.show()
+# 	quake_moms = quake_moments(data[l_bound:u_bound])
+# 	# x-axis of plot is time offset, which is the sum of all time_to_failure
+# 	# values of the first moments after the earth quakes.
+# 	dur = data[0,1] + sum([data[qm,1] for _,qm in quake_moms])
+# 	y_vals = np.linspace(0, dur, len(smpl))
+# 	ts = pd.Series(smpl[l_bound:u_bound,0], index=y_vals)
+# 	ts.plot()
+# 	plt.show()
 
-# # plott(data, 0, 20000000, 10000)
+# plott(data, 0, 20000000, 10000)
 
-# def sample(data, size=100000):
-#     assert size <= len(data)
+def sample(data, size=100000):
+    assert size <= len(data)
+    return np.random.choice(
+           data,
+           size,
+           replace=False
+    )
 
-#     return np.random.choice(
-#            data,
-#            size,
-#            replace=False
-#     )
+resolution = 4
 
-# # For a given ending position "u_bound", we split the last 150.000 values of "x" into 150 pieces of length 1000 each.
-# # From each piece, 16 features are extracted. This results in a feature matrix of dimension (150 time steps x 16 features). 
-# def features(data, u_bound=None, nr_subsets=150, subset_size=1000):
-#     nr_values = nr_subsets * subset_size
-#     u_bound   = u_bound or len(data)
-#     l_bound   = u_bound - nr_values
-#     assert l_bound >= 0
-#     # return np.array(data[l_bound:u_bound,0])
-#     # Reshaping and standardize approximately
-#     l = data[l_bound:u_bound].reshape(nr_subsets, -1)
-#     # Extracts features of sequences of full length 1000, of the last 100 values and finally also 
-#     # of the last 10 observations. 
-#     return stats(l)
-#     # return np.c_[
-#     # 	summary_statistics(l),
-#     # 	summary_statistics(l[:, -subset_size // 10:]),
-#     # 	summary_statistics(l[:, -subset_size // 100:]),
-#     # 	l[:, -1:]
-#     # ]
+# For a given ending position "u_bound", we split the last 150.000 values of "x" into 150 pieces of length 1000 each.
+# From each piece, 16 features are extracted. This results in a feature matrix of dimension (150 time steps x 16 features). 
+def features(data, u_bound):
+    l_bound = u_bound - 150000
+    assert l_bound >= 0
+    arr = np.array(data[l_bound:u_bound][::resolution])
+    return np.expand_dims(arr, 1)
+    # Reshaping and standardize approximately
+    # l = data[l_bound:u_bound].reshape(nr_subsets, -1)
+    # Extracts features of sequences of full length 1000, of the last 100 values and finally also 
+    # of the last 10 observations. 
+    # return stats(l)
+    # return np.c_[
+    # 	summary_statistics(l),
+    # 	summary_statistics(l[:, -subset_size // 10:]),
+    # 	summary_statistics(l[:, -subset_size // 100:]),
+    # 	l[:, -1:]
+    # ]
 
-# # We call "summary_statistics" three times, so the total number of features is 3 * 5 + 1 (last value) = 16
+# We call "summary_statistics" three times, so the total number of features is 3 * 5 + 1 (last value) = 16
 # nr_features = 4
 
-# # The generator randomly selects "batch_size" ending positions of sub-time series. For each ending position,
-# # the "time_to_failure" serves as target, while the features are created by the function "features".
-# def generator(data):
-#     l_bound     = 0
-#     u_bound     = len(data) - 1
-#     batch_size  = 32
-#     n_steps     = 1500
-#     step_length = 100
-#     while True:
-#         # Pick indices of ending positions
-#         rows = np.random.randint(l_bound + n_steps * step_length, u_bound, size=batch_size)
-#         # Initialize feature matrices and targets
-#         samples = np.zeros((batch_size, n_steps, nr_features))
-#         targets = np.zeros(batch_size, )
-#         for j, row in enumerate(rows):
-#             samples[j] = features(
-#                 data[:, 0],
-#                 u_bound     = row,
-#                 nr_subsets  = n_steps,
-#                 subset_size = step_length
-#             )
-#             targets[j] = data[row, 1]
-#         yield samples, targets
+# Generates 32 indices in the range [mi,ma) that are all at least
+# 150.000 values apart, such that the sequences they represent will never
+# overlap. This function is technically not guaranteed to terminate, but
+# will as long as parameters are decent.
+def indices(mi, ma, nr, min_dist):
+    while True:
+        vs = np.sort(np.random.randint(mi, ma, nr))
+        dists = np.all(vs[1:] - vs[:-1] >= min_dist)
+        return vs
+    
 
-# gen = generator(data)
+fst = features(
+    d[:, 0],
+    u_bound     = 74803393
+)
 
-# # Define model
-# # os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
-# from keras.models import Sequential
-# from keras.layers import Dense, GRU, LSTM
-# from keras.optimizers import adam
-# from keras.callbacks import ModelCheckpoint
 
-# # keras.layers.CuDNNGRU(units, kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', bias_initializer='zeros', kernel_regularizer=None, recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, return_sequences=False, return_state=False, stateful=False)
-# # keras.layers.      GRU(units, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', bias_initializer='zeros', kernel_regularizer=None, recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, dropout=0.0, recurrent_dropout=0.0, implementation=1, return_sequences=False, return_state=False, go_backwards=False, stateful=False, unroll=False, reset_after=False)
+# The generator randomly selects "batch_size" ending positions of sub-time series. For each ending position,
+# the "time_to_failure" serves as target, while the features are created by the function "features".
+def generator(data):
+    l_bound     = 0
+    u_bound     = len(data) - 1
+    n_steps     = 1500
+    step_length = 100
 
-# cb = [ModelCheckpoint("model.hdf5", monitor='val_loss', save_weights_only=False, period=3)]
+    batch_size  = 32
+    while True:
+        # Pick indices of ending positions
+        # rows = np.random.randint(l_bound + n_steps * step_length, u_bound, size=batch_size)
+        # rows = indices(0, len(d), batch_size, 150000)
+        rows = np.random.randint(0, len(data), batch_size)
+        # Initialize feature matrices and targets
+        samples = np.empty((batch_size, 150000//resolution, 1)) # np.zeros((batch_size, n_steps, nr_features))
+        targets = np.empty(batch_size, )
+        for j, row in enumerate(rows):
+            fts = features(
+                data[:, 0],
+                u_bound     = row
+            )
+            samples[j] = fts
+            targets[j] = data[row, 1]
+        yield samples, targets
+
+train_gen = generator(d[:-len(d)//5])
+valid_gen = generator(d[-len(d)//5:])
+
+
+
+
+
+
+
+
+
+
+
+
+def resnet_layer(model,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+    """2D Convolution-Batch Normalization-Activation stack builder
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-4))
+
+    if conv_first:
+        model.add(conv)
+        if batch_normalization:
+            model.add(BatchNormalization)
+        if activation is not None:
+            model.add(Activation(activation))
+    else:
+        if batch_normalization:
+            model(BatchNormalization)
+        if activation is not None:
+            model.add(Activation(activation))
+        model.add(conv(x))
+
+
+
+
+
+
+
+
+
+
+
+# Define model
+# os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+from keras.models import Sequential
+from keras.layers import Dense, GRU, LSTM, Conv1D, BatchNormalization, GlobalAveragePooling1D
+from keras.optimizers import adam
+from keras.callbacks import ModelCheckpoint
+
+# keras.layers.CuDNNGRU(units, kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', bias_initializer='zeros', kernel_regularizer=None, recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, return_sequences=False, return_state=False, stateful=False)
+# keras.layers.      GRU(units, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', bias_initializer='zeros', kernel_regularizer=None, recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, dropout=0.0, recurrent_dropout=0.0, implementation=1, return_sequences=False, return_state=False, go_backwards=False, stateful=False, unroll=False, reset_after=False)
+
+cb = [ModelCheckpoint("model.hdf5", monitor='val_loss', save_weights_only=False, period=3)]
+
+model = Sequential()
+
+model.add(Conv1D(128, kernel_size=8, strides=1, input_shape=(37500, 1)))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+
+model.add(Conv1D(128, kernel_size=5, strides=1))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+
+model.add(Conv1D(128, kernel_size=3, strides=1))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+
+model.add(GlobalAveragePooling1D())
+
+model.add(Dense(1))
+
+model.summary()
+
+# Compile and fit model
+model.compile(optimizer=adam(lr=0.0005), loss="mae")
+
+history = model.fit_generator(train_gen,
+                              steps_per_epoch=250,#n_train // batch_size,
+                              epochs=1,
+                              verbose=1,
+                              callbacks=cb,
+                              validation_data=valid_gen,
+                              validation_steps=25)#n_val
+
+
+
+
+
+
+
+
+
+# model = Sequential()
+# model.add(resnet_layer(model, 64))
+# model.add(resnet_layer(model, 64))
+# model.add(resnet_layer(model, 64))
+
+# model.add(resnet_layer(model, 128))
+# model.add(resnet_layer(model, 128))
+# model.add(resnet_layer(model, 128))
+
+# model.add(resnet_layer(model, 128))
+# model.add(resnet_layer(model, 128))
+# model.add(resnet_layer(model, 128))
+
+
+
+
+
+
+
+
+
+
 
 # model = Sequential()
 # # NVIDIA Only: model.add(CuDNNGRU(48, input_shape=(None, nr_features)))
-# model.add(GRU(48, input_shape=(None, 4))) # (150 (None mag ook), nr_features)))
+# model.add(GRU(48, input_shape=(37500,1))) # (150 (None mag ook), nr_features)))
 # model.add(Dense(10, activation='relu'))
 # model.add(Dense(1))
 
@@ -203,12 +346,12 @@ print("Loaded data in %s seconds." % (time() - s))
 # # Compile and fit model
 # model.compile(optimizer=adam(lr=0.0005), loss="mae")
 
-# history = model.fit_generator(gen,
+# history = model.fit_generator(train_gen,
 #                               steps_per_epoch=250,#n_train // batch_size,
 #                               epochs=1,
 #                               verbose=1,
 #                               callbacks=cb,
-#                               validation_data=gen,
+#                               validation_data=valid_gen,
 #                               validation_steps=25)#n_valid // batch_size)
 
 # submission = pd.read_csv('data/sample_submission.csv', index_col='seg_id', dtype={"time_to_failure": np.float32})
